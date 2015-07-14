5b70daecddcc646ed5711df1cf26fbd6cfd6c1ff
Shanna Chau <shannachau@gmail.com>
2015-07-14 16:52:44 -0700

    Generate product model

---
+++ db/migrate/20150714234924_create_products.rb
@@ -0,0 +1,15 @@
+class CreateProducts < ActiveRecord::Migration
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.string :description
+      t.float :price
+      t.integer :stock
+      t.boolean :active
+      t.string :photo_url
+      t.integer :merchant_id
+
+      t.timestamps null: false
+    end
+  end
+end
