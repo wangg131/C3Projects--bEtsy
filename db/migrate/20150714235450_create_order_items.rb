5732349e2009ea6247dc068d457cd8a14e98b03a
Shanna Chau <shannachau@gmail.com>
2015-07-14 16:55:14 -0700

    Generate order_item model

---
+++ db/migrate/20150714235450_create_order_items.rb
@@ -0,0 +1,12 @@
+class CreateOrderItems < ActiveRecord::Migration
+  def change
+    create_table :order_items do |t|
+      t.integer :product_id
+      t.integer :order_id
+      t.boolean :shipped
+      t.integer :merchant_id
+
+      t.timestamps null: false
+    end
+  end
+end
