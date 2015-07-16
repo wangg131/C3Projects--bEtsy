b0a8756b42b13f6ac138bd4f043c04851f6ce87b
Carly Jugler <carjug@gmail.com>
2015-07-15 22:01:26 -0700

    Adds working seeds for 4/6 models. Order and Order Items need to have table schemas that are current with the revised model for their seeds to populate correctly. Note: those models with a price are currently populated incorrectly with a value of 0-- integer or 0.0-- float. This might be because of the dollar sign that has been used in the csv file columns. Need to explore further :D

--- db/migrate/20150714235450_create_order_items.rb
+++ db/migrate/20150714235450_create_order_items.rb
@@ -3,8 +3,8 @@ class CreateOrderItems < ActiveRecord::Migration
     create_table :order_items do |t|
       t.integer :product_id
       t.integer :order_id
+      t.float :revenue
       t.boolean :shipped
-      t.integer :merchant_id
 
       t.timestamps null: false
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,34 @@ require 'csv'
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-CSV.foreach("db/seed_data_categories.csv", { encoding: "UTF-8", headers: true, header_converters: :symbol, converters: :all}) do |row|
-    Categories.create(row.to_hash)
+# NOTE: for some reason the prices aren't populating in the database -- this might be because we've entered data in with a $ when it's only expecting either an integer or a float, but not totally sure. Need to figure this out.
+# XD
+CSV.foreach("db/seed_data_products.csv", { encoding: "UTF-8", headers: true, header_converters: :symbol, converters: :all}) do |row|
+    Product.create(row.to_hash)
+end
+
+CSV.foreach("db/seed_data_merchants.csv", { encoding: "UTF-8", headers: true, header_converters: :symbol, converters: :all}) do |row|
+    Merchant.create(
+      name: row[:name],
+      email: row[:email],
+      password_digest: row[:password]
+      )
 end
 
 CSV.foreach("db/seed_data_reviews.csv", { encoding: "UTF-8", headers: true, header_converters: :symbol, converters: :all}) do |row|
-    Reviews.create(row.to_hash)
+    Review.create(row.to_hash)
 end
+
+CSV.foreach("db/seed_data_categories.csv", { encoding: "UTF-8", headers: true, header_converters: :symbol, converters: :all}) do |row|
+end
+
+# We need to have accurate table schemas for these migrations to work :D
+
+#CSV.foreach("db/seed_data_order_items.csv", { encoding: "UTF-8", headers: true, header_converters: :symbol, converters: :all}) do |row|
+#    OrderItem.create(row.to_hash)
+#end
+
+#CSV.foreach("db/seed_data_orders.csv", { encoding: "UTF-8", headers: true, header_converters: :symbol, converters: :all}) do |row|
+#    Order.create(row.to_hash)
+#end
 
