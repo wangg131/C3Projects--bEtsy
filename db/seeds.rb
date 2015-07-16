722387b22c3f168cceaf74e3b0a6e0ee64b9e55a
Carly Jugler <carjug@gmail.com>
2015-07-16 13:52:09 -0700

    Adds seed files.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+CSV.foreach("db/seed_data_products.csv", { encoding: "UTF-8", headers: true, header_converters: :symbol, converters: :all}) do |row|
+    Product.create(row.to_hash)
+end
+
+CSV.foreach("db/seed_data_merchants.csv", { encoding: "UTF-8", headers: true, header_converters: :symbol, converters: :all}) do |row|
+    Merchant.create(
+      name: row[:name],
+      email: row[:email],
+      password_digest: row[:password]
+      )
+end
+
+CSV.foreach("db/seed_data_reviews.csv", { encoding: "UTF-8", headers: true, header_converters: :symbol, converters: :all}) do |row|
+    Review.create(row.to_hash)
+end
+
+CSV.foreach("db/seed_data_categories.csv", { encoding: "UTF-8", headers: true, header_converters: :symbol, converters: :all}) do |row|
+end
+
+CSV.foreach("db/seed_data_order_items.csv", { encoding: "UTF-8", headers: true, header_converters: :symbol, converters: :all}) do |row|
+    OrderItem.create(row.to_hash)
+end
+
+CSV.foreach("db/seed_data_orders.csv", { encoding: "UTF-8", headers: true, header_converters: :symbol, converters: :all}) do |row|
+    Order.create(row.to_hash)
+end
+
