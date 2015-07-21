0b351ce573e568e248efb1baccea662e05d3974c
Shanna Chau <shannachau@gmail.com>
2015-07-21 10:02:30 -0700

    Changed migration to add quantity column. Fixed corresponding seed data:

--- db/migrate/20150714235450_create_order_items.rb
+++ db/migrate/20150714235450_create_order_items.rb
@@ -3,10 +3,11 @@ class CreateOrderItems < ActiveRecord::Migration
     create_table :order_items do |t|
       t.integer :product_id
       t.integer :order_id
+      t.integer :quantity
       t.float :revenue
       t.boolean :shipped
       t.integer :merchant_id
-      
+
       t.timestamps null: false
     end
   end
