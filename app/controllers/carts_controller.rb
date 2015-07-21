36b862fdb3a653300784e141545b474c2b39b223
Kari <kari.lonczak@gmail.com>
2015-07-21 15:27:37 -0700

    The cart adds the number of units that you have in the cart, not the items. The cart icon in the upper right corner totals the number of units in your cart also.

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -3,7 +3,18 @@ class CartsController < ApplicationController
   def show
     @order_items = current_order.order_items
 
+    calc_unit_total
     calc_order_total 
+  end
+
+  def calc_unit_total
+    @unit_total = 0
+
+    @order_items.each do |order_item|
+      @unit_total += order_item.quantity
+    end
+
+    return @unit_total
   end
 
   def calc_order_total 
