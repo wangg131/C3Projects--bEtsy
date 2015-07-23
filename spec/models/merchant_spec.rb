c541087e36c7c97fa4833b34143235ece337cb70
Carly Jugler <carjug@gmail.com>
2015-07-23 09:35:41 -0700

    Adds merchant model validation spec

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
-# RSpec.describe Merchant, type: :model do 
+RSpec.describe Merchant, type: :model do
 
-#   describe "model validations" do
-#     it "requires a name, email, and password and password_confirmation all the time" do
-#       merchant = Merchant.new
+  describe "model validations" do
+    it "requires a name, email, and password and password_confirmation all the time" do
+      merchant = Merchant.new
 
-#       expect(merchant).to_not be_valid
-#       expect(merchant.errors.keys).to include(:name,:email,:password,:password_confirmation)
-#     end
-#   end
+      expect(merchant).to_not be_valid
+      expect(merchant.errors.keys).to include(:name,:email,:password,:password_confirmation)
+    end
+  end
 
-# end
+end
