bc29535fd6161f77d6953b6961505f43b49e12fc
Carly Jugler <carjug@gmail.com>
2015-07-23 13:31:19 -0700

    Adds cart route specs

--- spec/routing/cart_spec.rb
+++ spec/routing/cart_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe CartsController, type: :routing do
+RSpec.describe "routes for Carts", type: :routing do
 
+  it "routes /cart to the carts controller" do
+    expect(get("/cart")).to route_to("carts#show")
+  end
+
+  it "route /add_to_cart is routable" do
+    expect(post("/add_to_cart/1")).to be_routable
+  end
 end
