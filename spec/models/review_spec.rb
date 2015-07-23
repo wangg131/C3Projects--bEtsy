b70af46c30a1a697d047444b5822e8e0553b91d5
Carly Jugler <carjug@gmail.com>
2015-07-23 09:52:45 -0700

    Adds validations rspecs for the review model.

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
-# RSpec.describe Review, type: :model do 
+RSpec.describe Review, type: :model do
 
-#   describe "model validations" do
-#     it "requires content, rating, and product_id" do
-#       review = Review.new
+  describe "model validations" do
+    it "requires content, rating, and product_id" do
+      review = Review.new
 
-#       expect(review).to_not be_valid
-#       expect(review.errors.keys).to include(:content,:rating,:product_id)
-#     end
-#   end
+      expect(review).to_not be_valid
+      expect(review.errors.keys).to include(:content,:rating,:product_id)
+    end
+  end
 
-# end
+end
