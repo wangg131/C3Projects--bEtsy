88658e95189245435291e762e4325613ae7a36e1
Kari <kari.lonczak@gmail.com>
2015-07-23 10:00:07 -0700

    Added spec to test associations. They're green. Wooo done with order_item model specs.

--- spec/models/order_item_spec.rb
+++ spec/models/order_item_spec.rb
@@ -9,6 +9,12 @@ RSpec.describe OrderItem, type: :model do
       expect(order_item).to_not be_valid
       expect(order_item.errors.keys).to include(:product_id, :order_id, :revenue, :quantity)
     end
+
+    it "belongs to an order, a product and a merchant" do
+      expect(OrderItem.reflect_on_association(:order).macro).to eq(:belongs_to)
+      expect(OrderItem.reflect_on_association(:product).macro).to eq(:belongs_to)
+      expect(OrderItem.reflect_on_association(:merchant).macro).to eq(:belongs_to)
+    end
   end
 
 end
