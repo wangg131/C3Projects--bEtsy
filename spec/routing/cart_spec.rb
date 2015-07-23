4b0b0004bf8cc988fa9f872c4208560bc53f52f8
Carly Jugler <carjug@gmail.com>
2015-07-23 13:32:51 -0700

    Adds negative check for add_to_cart route

--- spec/routing/cart_spec.rb
+++ spec/routing/cart_spec.rb
@@ -9,4 +9,8 @@ RSpec.describe "routes for Carts", type: :routing do
   it "route /add_to_cart is routable" do
     expect(post("/add_to_cart/1")).to be_routable
   end
+
+  it "does not route to a cart without an :id" do
+    expect(post("/add_to_cart/")).not_to be_routable
+  end
 end
