bedd3480343d1f359799d0b82b7c5c2a9787dcbf
marleigh <camarleigh@gmail.com>
2015-07-23 15:59:49 -0700

    all same page spec count = 42.

--- spec/controllers/cart_spec.rb
+++ spec/controllers/cart_spec.rb
@@ -2,30 +2,30 @@ require 'rails_helper'
 
 
 RSpec.describe CartsController, type: :controller do
-  describe "GET #show" do
-    it "responds successfully with an HTTP 200 status code" do
-      get :show
+ describe "GET #show" do
+   it "responds successfully with an HTTP 200 status code" do
+     get :show
 
-      expect(response).to be_success
-      expect(response).to have_http_status(200)
-    end
+     expect(response).to be_success
+     expect(response).to have_http_status(200)
+   end
 
-    # context "you can adjust cart item quantities in the cart #show view" do
-    #   describe "calculates new totals in cart" do
-    #     it "calculates total units" do
-    #       current_order = Order.create
-    #       current_order.order_items << OrderItem.
-    #       @order_items = current_order.order_items
+   # context "you can adjust cart item quantities in the cart #show view" do
+   #   describe "calculates new totals in cart" do
+   #     it "calculates total units" do
+   #       current_order = Order.create
+   #       current_order.order_items << OrderItem.
+   #       @order_items = current_order.order_items
 
-    #       calc_unit_total
+   #       calc_unit_total
 
-    #       expect(@unit_total).to_eq 7
-    #     end
+   #       expect(@unit_total).to_eq 7
+   #     end
 
-        # it "calculates total dollars" do
-        # end
+       # it "calculates total dollars" do
+       # end
 
-      # end
-    end
-  end
+     # end
+   # end
+ end
 end
