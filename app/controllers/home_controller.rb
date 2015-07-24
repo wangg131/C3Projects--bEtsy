f25f1c528028290f87c7bd367da6b7781e75a20b
Kari <kari.lonczak@gmail.com>
2015-07-23 17:31:15 -0700

    Updated home page to display the first 3 and last 3 products that are active.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,9 @@
 class HomeController < ApplicationController
   def index
-    @product_first_row = Product.first(3)
-    @product_second_row = Product.last(3)
+
+    @active_products = Product.where(active: true)
+    @product_first_row = @active_products.first(3)
+    @product_second_row = @active_products.last(3)
   end
 
 end
