64912cd51dc95f3fe64f2582b6a7e62d84c7b92e
Brenna <brennaleker@gmail.com>
2015-08-20 09:44:54 -0700

    edited views and orders controller to display shipping options

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -43,14 +43,17 @@ class OrdersController < ApplicationController
     else
       @package_size = "small"
     end
-    redirect_to results_path if params[:estimate]
+    render :action => 'estimate' if params[:estimate_button]
   end
 
   def results
     @order_items = current_order.order_items
     estimate_request = params[:estimate]
     @shipment_response = HTTParty.get("http://localhost:3001/", :body => estimate_request)
+    @usps = @shipment_response[1]
+    @ups = @shipment_response[0]
     calc_order_total
+    
   end
 
   def calc_order_total
@@ -218,4 +221,5 @@ class OrdersController < ApplicationController
   def redacted_cc(credit_card)
     credit_card.chars.last(4).join
   end
+
 end
